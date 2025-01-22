37bc5f95b21efda1028adcf5e56f64397adf313e
Anton Kolo <kvanton7@gmail.com>
2025-01-22 12:47:08 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="block mt-0 mb-[2rem] mx-auto"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
+  </header>
+</template>
